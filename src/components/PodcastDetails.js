077d27cdca7696a5da138024ab91943ec31c553c
Anesu Muusha <anesumuusha@gmail.com>
2024-09-25 10:06:03 +0200

    Commented out the navBar

--- src/components/PodcastDetails.js
+++ src/components/PodcastDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
 
 function PodcastDetails({ onPlayEpisode }) {
   const { id } = useParams();
